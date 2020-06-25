342b49c4ddbb09713f8bd964bf76fc85bcb72747
Aadam <aadimator@gmail.com>
2020-06-25 15:54:00 +0500

    feat: createPeerMatrix

--- src/NetMSA.jl
+++ src/NetMSA.jl
@@ -1,5 +1,14 @@
 module NetMSA
 
 # Write your package code here.
+function createPeerMatrix(inputStrings::Array{String,1} ):: Matrix{Union{Missing, Char}}
+		col_size = size(inputStrings, 1);
+		row_size = max([length(s) for s in inputStrings]...);
+		peer_matrix = Matrix{Union{Missing, Char}}(missing, row_size, col_size);
+		for (i, s) in enumerate(inputStrings)
+			peer_matrix[1: length(s), i] = collect(s);
+		end
+		return peer_matrix;
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,23 @@ using Test
 
 @testset "NetMSA.jl" begin
     # Write your tests here.
+    S1 = "abcbcdem";
+    S2 = "acbcfg";
+    S3 = "abchimn";
+    S4 = "abcbcjkm";
+
+    L = [S1, S2, S3, S4];
+    M = [
+        ['a' 'a' 'a' 'a']
+        ['b' 'c' 'b' 'b']
+        ['c' 'b' 'c' 'c']
+        ['b' 'c' 'h' 'b']
+        ['c' 'f' 'i' 'c']
+        ['d' 'g' 'm' 'j']
+        ['e' missing 'n' 'k']
+        ['m' missing missing 'm']
+        ]
+
+    @test isequal(NetMSA.createPeerMatrix(L), M);
+
 end
