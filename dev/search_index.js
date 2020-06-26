d4dec37954329e1991e1178eab0a4554b9314849
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-06-26 09:49:10 +0000

    build based on 1fb9611

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#","page":"Home","title":"Home","text":"CurrentModule = NetMSA","category":"page"},{"location":"#NetMSA-1","page":"Home","title":"NetMSA","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [NetMSA]","category":"page"}]
+}
