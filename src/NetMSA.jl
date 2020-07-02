35d182dd9cab063bac8dfe7a362037ef6db3476c
Aadam <aadimator@gmail.com>
2020-07-02 15:18:20 +0500

    docs: update docs and add test cases

--- src/NetMSA.jl
+++ src/NetMSA.jl
@@ -8,7 +8,7 @@ using StatsBase
 
 export createPeerMatrix, matrixalignment
 
-@doc raw"""
+"""
 Store the position of a given particle. Position ``x_s_i (r)`` of the particle
 ``p_s_i`` is defined by using the row ``r`` that contains the symbol ``s_i`` as
 well as locations of the symbol ``s_i`` in the different columns (indexes of the
